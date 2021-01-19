360b9e61d1da3c8b7bf2b2fbbfa92c2607033d48
Simeon Bukurov <71982116+SwagerStorm@users.noreply.github.com>
2021-01-19 21:24:38 +0200

    Add files via upload

---
+++ Zadacha7.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Array = new string[] { "13", "5", "32", "0", "4", "19", "23", "22", "52" };
+            int[] IntArray = Array.Select(int.Parse).Where(n => n % 2 == 0).OrderByDescending(n => n).ToArray();
+            Console.WriteLine(String.Join(", ", IntArray));
+        }
+    }
+}
